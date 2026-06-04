c05c53d185b2867a55bd0a9b17d823942d1b87a1
Gaboouu <99047760+xGabou@users.noreply.github.com>
2026-06-04 07:20:10 -0400

    Downgrade Minecraft version to 1.20.4, update dependencies, and rename Forge class for compatibility

--- neoforge/src/main/java/net/Gabou/gaboulibs/neoforge/GaboulibsNeoForge.java
+++ neoforge/src/main/java/net/Gabou/gaboulibs/neoforge/GaboulibsNeoForge.java
@@ -1,11 +1,11 @@
-package net.Gabou.gaboulibs.forge;
+package net.Gabou.gaboulibs.neoforge;
 
 import net.Gabou.gaboulibs.Gaboulibs;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Gaboulibs.MOD_ID)
-public final class GaboulibsForge {
-    public GaboulibsForge() {
+public final class GaboulibsNeoForge {
+    public GaboulibsNeoForge() {
         // Run our common setup.
         Gaboulibs.init();
     }
