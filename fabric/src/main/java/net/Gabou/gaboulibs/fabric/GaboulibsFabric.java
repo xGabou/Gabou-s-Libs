e07c0d8a71abaa9897098812f57eb5493da3cf0d
Gaboouu <99047760+xGabou@users.noreply.github.com>
2026-08-30 20:31:19 -0400

    Remove custom authentication checks

--- fabric/src/main/java/net/Gabou/gaboulibs/fabric/GaboulibsFabric.java
+++ fabric/src/main/java/net/Gabou/gaboulibs/fabric/GaboulibsFabric.java
@@ -1,8 +1,6 @@
 package net.Gabou.gaboulibs.fabric;
 
 import net.Gabou.gaboulibs.Gaboulibs;
-import net.Gabou.gaboulibs.ModNetworking;
-import net.Gabou.gaboulibs.fabric.auth.Identity2FabricNetworkingPlatform;
 import net.fabricmc.api.ModInitializer;
 
 public final class GaboulibsFabric implements ModInitializer {
@@ -11,7 +9,6 @@ public final class GaboulibsFabric implements ModInitializer {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
-        ModNetworking.setPlatform(new Identity2FabricNetworkingPlatform());
         // Run our common setup.
         Gaboulibs.init();
     }
--- neoforge/src/main/java/net/Gabou/gaboulibs/neoforge/GaboulibsForge.java
+++ neoforge/src/main/java/net/Gabou/gaboulibs/neoforge/GaboulibsForge.java
@@ -1,8 +1,6 @@
 package net.Gabou.gaboulibs.neoforge;
 
 import net.Gabou.gaboulibs.Gaboulibs;
-import net.Gabou.gaboulibs.ModNetworking;
-import net.Gabou.gaboulibs.neoforge.auth.Identity2NeoForgeNetworkingPlatform;
 import net.Gabou.gaboulibs.neoforge.client.GaboulibsNeoForgeClient;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
@@ -12,7 +10,6 @@ import net.neoforged.fml.loading.FMLEnvironment;
 public final class GaboulibsForge {
     public GaboulibsForge() {
         // Run our common setup.
-        ModNetworking.setPlatform(new Identity2NeoForgeNetworkingPlatform());
         Gaboulibs.init();
         if (FMLEnvironment.getDist() == Dist.CLIENT) {
             GaboulibsNeoForgeClient.initialize();
